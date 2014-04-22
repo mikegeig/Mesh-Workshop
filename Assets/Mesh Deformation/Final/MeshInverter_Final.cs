7fc8255636760b3b62268cb4eb1605d778c886fc
Mike Geig <mikegeig@gmail.com>
2014-04-22 16:16:48 -0400

    Added mesh inverter

---
+++ Assets/Mesh Deformation/Final/MeshInverter_Final.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshInverter_Final : MonoBehaviour 
+{
+	Mesh mesh;
+	MeshFilter filter;
+	
+	void Start () 
+	{
+		filter = GetComponent<MeshFilter>();
+		mesh = filter.mesh;
+
+		int[] tris = mesh.triangles;
+		
+		for(int i = 0; i < tris.Length; i += 3)
+		{
+			int x = tris[i];
+			tris[i] = tris[i + 2];
+			tris[i + 2] = x;
+		}
+		mesh.triangles = tris;
+		mesh.RecalculateNormals();
+		mesh.RecalculateBounds();
+	}
+}
---
+++ Assets/Mesh Deformation/Scripts/MeshInverter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshInverter : MonoBehaviour 
+{
+	Mesh mesh;
+	MeshFilter filter;
+	
+	void Start () 
+	{
+		filter = GetComponent<MeshFilter>();
+		mesh = filter.mesh;
+	}
+}
